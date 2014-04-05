2ab74a8f8b9a51d1a19e669493468488d77dfd95
blobs12593 <blobs12593@hotmail.com>
2014-04-05 15:10:30 -0400

    BrokenButBetter

--- Quotes2/Controllers/Quotes2Controller.cs
+++ Quotes2/Controllers/Quotes2Controller.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Quotes2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Quotes2.Controllers
 {
@@ -14,42 +16,34 @@ namespace Quotes2.Controllers
     {
         private Quotes2Context db = new Quotes2Context();
 
-        // GET: /Quotes2/
-        public ActionResult Index(string Search, int? id, string delete, string doNotAddID)
-        {
-            HttpCookie myCookie = Request.Cookies.Get("Quote");
-            ViewBag.Cookie = false;
+        private UserManager<ApplicationUser> manager;
 
-            // Prevent accidental hiding
-            if (doNotAddID == "Search" || doNotAddID == "Clear Search")
+        public Quotes2Controller()
+        {
+            manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+        // GET: /Quotes2/
+        public ActionResult Index(string Search, int? id, string delete, string doNotAddID, string viewMine)
+        {
+            if (viewMine == "View My Quotes")
             {
-                id = null;
+                return View(manager);
             }
-            // If Unhide All was pressed
-            if (delete == "Unhide All")
-            {
-                myCookie.Expires = DateTime.Now.AddHours(-1);
-                Response.Cookies.Add(myCookie);
-                if (String.IsNullOrEmpty(Search))
-                {
-                    ViewBag.ClearSearch = false;
-                    var quotations = db.Quotations.Include(q => q.Category);
-                    return View(quotations.ToList());
-                }
-                else
-                {
-                    ViewBag.ClearSearch = true;
-                    var quotations = db.Quotations.Include(q => q.Category).Where(q => q.Category.Name.Contains(Search) || q.Quote.Contains(Search) || q.Author.Contains(Search));
-                    return View(quotations.ToList());
-                }
-            }
-            // If unhide all was not pressed
             else
             {
-                // If there is no cookie and no cookie going to be created
-                if (Request.Cookies.Get("Quote") == null && id == null)
+                HttpCookie myCookie = Request.Cookies.Get("Quote");
+                ViewBag.Cookie = false;
+
+                // Prevent accidental hiding
+                if (doNotAddID == "Search" || doNotAddID == "Clear Search")
                 {
-                    // Uncookied results
+                    id = null;
+                }
+                // If Unhide All was pressed
+                if (delete == "Unhide All")
+                {
+                    myCookie.Expires = DateTime.Now.AddHours(-1);
+                    Response.Cookies.Add(myCookie);
                     if (String.IsNullOrEmpty(Search))
                     {
                         ViewBag.ClearSearch = false;
@@ -63,44 +57,65 @@ namespace Quotes2.Controllers
                         return View(quotations.ToList());
                     }
                 }
-                // If the cookie does exist or will exist
+                // If unhide all was not pressed
                 else
                 {
-                    ViewBag.Cookie = true;
-                    // Check if add value was requested to existing cookie
-                    if (Request.Cookies.Get("Quote") != null && id != null)
+                    // If there is no cookie and no cookie going to be created
+                    if (Request.Cookies.Get("Quote") == null && id == null)
                     {
-                        myCookie.Value = myCookie.Value + " " + id.ToString();
+                        // Uncookied results
+                        if (String.IsNullOrEmpty(Search))
+                        {
+                            ViewBag.ClearSearch = false;
+                            var quotations = db.Quotations.Include(q => q.Category);
+                            return View(quotations.ToList());
+                        }
+                        else
+                        {
+                            ViewBag.ClearSearch = true;
+                            var quotations = db.Quotations.Include(q => q.Category).Where(q => q.Category.Name.Contains(Search) || q.Quote.Contains(Search) || q.Author.Contains(Search));
+                            return View(quotations.ToList());
+                        }
                     }
-                    // Check if a cookie needs to be created
-                    else if (id != null)
-                    {
-                        myCookie = new HttpCookie("Quote", id.ToString());
-                    }
-                    myCookie.Expires = DateTime.Now.AddDays(2);
-                    Response.Cookies.Add(myCookie);
-
-                    // Grab the cookies values and convert them to integers
-                    String[] allHidden = myCookie.Value.Split(' ');
-                    int[] allHiddenNums = new int[allHidden.Length];
-                    int counter = 0;
-                    foreach (string value in allHidden)
-                    {
-                        allHiddenNums[counter] = int.Parse(value);
-                        counter += 1;
-                    }
-
-                    if (String.IsNullOrEmpty(Search))
-                    {
-                        ViewBag.ClearSearch = false;
-                        var quotations = db.Quotations.Include(q => q.Category).Where(q => !allHiddenNums.Contains(q.QuotationID));
-                        return View(quotations.ToList());
-                    }
+                    // If the cookie does exist or will exist
                     else
                     {
-                        ViewBag.ClearSearch = true;
-                        var quotations = db.Quotations.Include(q => q.Category).Where(q => q.Category.Name.Contains(Search) || q.Quote.Contains(Search) || q.Author.Contains(Search));
-                        return View(quotations.ToList());
+                        ViewBag.Cookie = true;
+                        // Check if add value was requested to existing cookie
+                        if (Request.Cookies.Get("Quote") != null && id != null)
+                        {
+                            myCookie.Value = myCookie.Value + " " + id.ToString();
+                        }
+                        // Check if a cookie needs to be created
+                        else if (id != null)
+                        {
+                            myCookie = new HttpCookie("Quote", id.ToString());
+                        }
+                        myCookie.Expires = DateTime.Now.AddDays(2);
+                        Response.Cookies.Add(myCookie);
+
+                        // Grab the cookies values and convert them to integers
+                        String[] allHidden = myCookie.Value.Split(' ');
+                        int[] allHiddenNums = new int[allHidden.Length];
+                        int counter = 0;
+                        foreach (string value in allHidden)
+                        {
+                            allHiddenNums[counter] = int.Parse(value);
+                            counter += 1;
+                        }
+
+                        if (String.IsNullOrEmpty(Search))
+                        {
+                            ViewBag.ClearSearch = false;
+                            var quotations = db.Quotations.Include(q => q.Category).Where(q => !allHiddenNums.Contains(q.QuotationID));
+                            return View(quotations.ToList());
+                        }
+                        else
+                        {
+                            ViewBag.ClearSearch = true;
+                            var quotations = db.Quotations.Include(q => q.Category).Where(q => q.Category.Name.Contains(Search) || q.Quote.Contains(Search) || q.Author.Contains(Search));
+                            return View(quotations.ToList());
+                        }
                     }
                 }
             }
@@ -122,6 +137,7 @@ namespace Quotes2.Controllers
         }
 
         // GET: /Quotes2/Create
+        [Authorize]
         public ActionResult Create()
         {
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name");
---
+++ Quotes2/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Quotes2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Quotes2.Models.Quotes2Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Quotes2.Models.Quotes2Context";
+        }
+
+        protected override void Seed(Quotes2.Models.Quotes2Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
