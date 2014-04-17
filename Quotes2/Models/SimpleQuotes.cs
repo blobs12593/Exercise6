9411609f4d7cf18926122ca0b18c53acaf6818c3
blobs12593 <blobs12593@hotmail.com>
2014-04-17 10:58:07 -0400

    Exercise7_Start

---
+++ Quotes2/Models/SimpleQuotes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quotes2.Models
+{
+    public class SimpleQuotes
+    {
+        public string Quote { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+    }
+}
